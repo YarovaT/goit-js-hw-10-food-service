30c43588dbc7635515323e0688d731df497b303e
Tetiana Yarova <andronikova11@gmail.com>
2021-05-12 10:31:26 +0300

    Создание шаблона

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+import cardMenuTml from './templates/menu.hbs';
+import cards from './menu.json';
 import './sass/main.scss';
+
+const menuContainer = document.querySelector('.js-menu');
+
+const cardsMenu = createCardsMenu(cards);
+
+menuContainer.insertAdjacentElement('beforeend', cardsMenu);
+
+function createCardsMenu(cards) {
+  return cards.map(cardMenuTml).join('');
+}
